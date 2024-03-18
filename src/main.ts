5b801696612de59ff33af704f36d180c24dd9005
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-03-18 02:25:03 +0100

    Update zone.js import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
