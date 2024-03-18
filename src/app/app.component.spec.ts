a093ffa8a0cfb7229fb2f938b504649842dedc9b
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-03-18 02:17:23 +0100

    Update Angular project configuration and file names

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,11 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bowling-app'`, () => {
+  it(`should have as title 'angular-bowling-scorekeeper'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bowling-app');
+    expect(app.title).toEqual('angular-bowling-scorekeeper');
   });
-
-
 });
