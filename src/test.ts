6a3bfbdc1405062dc10948605ca29cda5349c9fa
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-03-18 02:56:02 +0100

    Update test file to include specific test modules

--- src/test.ts
+++ src/test.ts
@@ -1,30 +1,27 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import './load-tests';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+
 // Then we find all the tests.
-//const context = require.context('./', true, /\.spec\.ts$/);
+const context = [
+  require('./app/home/home.component.spec.ts'),
+  require('./app/new-game/new-game.component.spec.ts'),
+  require('./app/previous-scores/previous-scores.component.spec.ts'),
+  require('./app/app.component.spec.ts'),
+  require('./app/score-calculator.service.spec.ts'),
+  // add more require statements for each of your test files...
+];
+
 // And load the modules.
-//context.keys().map(context);
+context.forEach((module) => module);
